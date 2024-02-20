281f41623c1c6cf5fc02a3e4b01d8ae32fd5306a
Myron Dcunha <67193041+Myron2812@users.noreply.github.com>
2024-02-20 16:18:17 +0530

    Attached nvim clipboard to Windows clipboard
    Attaching the nvim clipboard to Windows clipboard(clip.exe) lets the user copy, cut and paste between Windows apps(like Notepad) and nvim.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,35 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+--
+local is_wsl = vim.fn.has('wsl') == 1
+-- local is_mac = vim.fn.has("macunix") == 1
+-- local is_linux = not is_wsl and not is_mac
 
-vim.opt.relativenumber = false -- Relative line numbers
+local opt = vim.opt
+
+opt.autowrite = false
+opt.confirm = true
+opt.breakindent = true
+opt.cindent = true
+opt.visualbell = false
+opt.relativenumber = false
+
+-- WSL Clipboard support
+if is_wsl then
+  -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
+  -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
